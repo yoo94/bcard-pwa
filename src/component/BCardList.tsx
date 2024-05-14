114da2553e7bff46e9ec253e0ae43547274320ed
yoo94 <jaeseok9405@gmail.com>
2024-05-14 15:19:48 +0900

    -search

--- src/component/BCardList.tsx
+++ src/component/BCardList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./BCardList.css"
 import BcardItem from "./BcardItem";
 
@@ -13,11 +14,37 @@ interface BCardListProps {
 }
 
 const BCardList: React.FC<BCardListProps> = ({ data }) => {
+    const [search, setSearch] = useState<string>("");
+
+    const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
+
+    const getFilteredData = () => {
+        if (search === "") return data
+        return data.filter((item) => {
+            return (item.name.includes(search) || item.hpNum.includes(search))
+        })
+    }
+
+    const filteredData = getFilteredData();
+
     return (
         <div>
-            {data.map((item) => {
-                return <BcardItem key={item.id} {...item} />
-            })}
+            <div className="BCardList">
+                <input
+                    className="SearchBar"
+                    placeholder="찾을 사람이나 이름을 입력하세요"
+                    value={search}
+                    onChange={onChangeSearch}
+                />
+                <div className="list_wrapper">
+                    {filteredData.map((item) => {
+                        return <BcardItem key={item.id} {...item} />
+                    })}
+                </div>
+            </div>
+
         </div>
     );
 }
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,16 +1,15 @@
 import './Header.css';
-    
+
 
 interface headerObj {
-    title:string,
+    title: string,
 }
 
-const Header = (obj : headerObj) => {
-    return(
+const Header = (obj: headerObj) => {
+    return (
         <header className='Header'>
             <h2 className='header_center'>{obj.title}</h2>
         </header>
-
     )
 };
 export default Header;
