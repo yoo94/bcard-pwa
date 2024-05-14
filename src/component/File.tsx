096db66c4d3071cb80430e420a90cfc9da5e8b8c
YOOJAESEOK <ahy0119@naver.com>
2024-05-14 19:09:17 +0900

    - 파일 미리보기

--- src/component/File.tsx
+++ src/component/File.tsx
@@ -1,21 +1,43 @@
-import { useRef } from 'react';
-
+import { useRef, useState } from 'react';
+import "./File.css"
 const File = () => {
+  interface EditorDataType {
+    file?: string | null;
+  }
   const fileInputRef = useRef<HTMLInputElement>(null);
-
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [postImg, setPostImg] = useState<EditorDataType>({ file: null });
+  const [previewImg, setPreviewImg] = useState<string>(''); // Changed to string
+  
+  function uploadFile(e: React.ChangeEvent<HTMLInputElement>) {
+    const fileArr = e.target.files;
+    if (fileArr && fileArr.length > 0) {
+      setPostImg({ file: URL.createObjectURL(fileArr[0]) }); // Store the file URL
+      
+      const fileRead = new FileReader();
+      fileRead.onload = function() {
+        const result = fileRead.result;
+        if (typeof result === 'string') {
+          setPreviewImg(result);
+        }
+      };
+      
+      fileRead.readAsDataURL(fileArr[0]); // Read the file as data URL
+    }
+  }
+  
   const handleFileSectionClick = () => {
     fileInputRef.current?.click();
   };
 
-
   return (
-    <div>
-      <label htmlFor=""></label>
+    <div className='File'>
       <section className="file_section" onClick={handleFileSectionClick}>
+        <img className="preview" src={previewImg} />
         <div>
-          <label htmlFor="file">+ 명함첨부</label>
+          <label htmlFor="file">+ 명함첨부</label> {/* Added id for file input */}
           <div className="hidden">
-            <input ref={fileInputRef} type="file" multiple accept="image/*" onChange={() => { }} />
+            <input id="file" ref={fileInputRef} type="file" multiple accept="image/*" onChange={uploadFile} />
           </div>
         </div>
       </section>
